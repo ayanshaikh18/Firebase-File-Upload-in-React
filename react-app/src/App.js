6ad9caa9dfe8ae761d6c06c2290c2832f8bb62f1
ayanshaikh18 <mahammadayan18@gmail.com>
2021-05-30 15:51:37 +0530

    done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import { storage } from "./firebase/firebase";
 
 function App() {
+  const [imageAsFile, setImageAsFile] = useState("");
+  const [imageAsUrl, setImageAsUrl] = useState();
+
+  const handleImageAsFile = (e) => {
+    const image = e.target.files[0];
+    setImageAsFile((imageFile) => image);
+  };
+
+  const handleFireBaseUpload = (e) => {
+    e.preventDefault();
+    console.log("start of upload");
+    console.log(imageAsFile);
+    const uploadTask = storage
+      .ref(`/images/${imageAsFile.name}`)
+      .put(imageAsFile);
+    uploadTask.on(
+      "state_changed",
+      (snapShot) => {
+        //takes a snap shot of the process as it is happening
+        console.log(snapShot);
+      },
+      (err) => {
+        //catches the errors
+        console.log(err);
+      },
+      () => {
+        // gets the functions from storage refences the image storage in firebase by the children
+        // gets the download url then sets the image from firebase as the value for the imgUrl key:
+        storage
+          .ref("images")
+          .child(imageAsFile.name)
+          .getDownloadURL()
+          .then((fireBaseUrl) => {
+            setImageAsUrl(fireBaseUrl);
+            console.log(imageAsUrl);
+          });
+      }
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex justify-content-center align-items-center">
+      <div className="container-fluid">
+        <div className="row mt-3 justify-content-center">
+          <div className="card w-50 mt-5 shadow-lg">
+            <div className="card-header text-center bg-white">
+              <h3>File Upload in react with firebase</h3>
+            </div>
+            <div className="card-body">
+              <form onSubmit={handleFireBaseUpload}>
+                <div className="input-group">
+                  <input
+                    onChange={handleImageAsFile}
+                    type="file"
+                    className="form-control"
+                    accept="image/*"
+                    required
+                  />
+                </div>
+                <br />
+                <div className="justify-content-center input-group">
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </div>
+              </form>
+              <br />
+              <div className="row text-center justify-content-center d-flex">
+                {imageAsUrl && (
+                  <>
+                  <h5>Uploaded Image :-</h5>
+                  <img
+                    src={imageAsUrl}
+                    style={{ height: "250px", width: "250px" }}
+                  />
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
